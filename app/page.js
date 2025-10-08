cf094b44c8c20e4085b7931059f54e18a3579ccf
Itteba Gilani <itteba1@gmail.com>
2025-10-08 11:02:24 -0400

    clerk authentication

--- app/page.js
+++ app/page.js
@@ -1,11 +1,9 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
 import ChatInputBox from "./_components/ChatInputBox";
 
 export default function Home() {
-  const { setTheme } = useTheme();
 
   return (
     <>
